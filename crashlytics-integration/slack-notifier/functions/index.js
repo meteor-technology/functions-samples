6811cba63a2fd766bf536ef97e9e0f1bf3bd2042
Shumpei Urabe <urabe@meteor.ne.jp>
2020-03-13 19:30:25 +0900

    Add regression nortify

--- crashlytics-integration/slack-notifier/functions/index.js
+++ crashlytics-integration/slack-notifier/functions/index.js
@@ -11,7 +11,7 @@ function notifySlack(slackMessage) {
     method: 'POST',
     uri: functions.config().slack.webhook_url,
     body: {
-      text: slackMessage,
+      slackMessage,
     },
     json: true,
   });
@@ -57,6 +57,46 @@ exports.postOnNewIssue = functions.crashlytics.issue().onNew(async (issue) => {
   console.log(`Posted new issue ${issueId} successfully to Slack`);
 });
 // [END on_new_issue]
+
+exports.postOnRegressedIssue = functions.crashlytics.issue().onRegressed(async (issue) => {
+  const issueId = issue.issueId;
+  const issueTitle = issue.issueTitle;
+  const appName = issue.appInfo.appName;
+  const appPlatform = issue.appInfo.appPlatform;
+  const latestAppVersion = issue.appInfo.latestAppVersion;
+  const resolvedTime = issue.resolvedTime;
+
+  const messageBody = {
+    "username": "Firebase Crashlytics", // This will appear as user name who posts the message
+    "text": `${new Date(resolvedTime).toString()} 以来、再び問題が発生しました`,
+    "icon_emoji": ":firebase:", // User icon, you can also use custom icons here
+    "attachments": [{ // this defines the attachment block, allows for better layout usage
+      "color": "#ff8c00", // color of the attachments sidebar.
+      "fields": [ // actual fields
+        {
+          "title": "Issue Title", // Custom field
+          "text": issueTitle
+        },
+        {
+          "title": "Issue Number",
+          "text": issueId
+        },
+        {
+          "title": "App Name",
+          "text": appName
+        },
+        {
+          "title": "App Version",
+          "text": `${latestAppVersion} on ${appPlatform}`
+        }
+      ]
+    }]
+  };
+
+
+  await notifySlack(messageBody);
+  console.log(`Posted regressed issue ${issueId} successfully to Slack`);
+});
 
 exports.postOnVelocityAlert = functions.crashlytics.issue().onVelocityAlert(async (issue) => {
   const issueId = issue.issueId;
